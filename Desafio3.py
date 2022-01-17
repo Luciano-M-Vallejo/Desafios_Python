5940618e4830e233fa73625f378512139591472e
Luciano Manuel Vallejo <73795741+Luciano-M-Vallejo@users.noreply.github.com>
2022-01-17 12:11:47 -0300

    Desafio 3 actualizado

--- Desafio3.py
+++ Desafio3.py
@@ -8,52 +8,52 @@
 # En caso de no introducir una opción válida, el programa informará de que no es correcta.
 
 
-# input_1 = int(input('Ingresa el primer numero: '))
-# input_2 = int(input('Ingresa el segundo numero: '))
-# opciones = ['Sumar', 'Restar', 'Multiplicar', 'Salir' ]
+input_1 = int(input('Ingresa el primer numero: '))
+input_2 = int(input('Ingresa el segundo numero: '))
+opciones = ['Sumar', 'Restar', 'Multiplicar', 'Salir' ]
 
-# for i, op in enumerate(opciones):
-#     print(i+1, op)
-# activar = True
-# while activar:
-#     input_3 = int(input('Elija una Opcion: '))
-#     if input_3 == 1:
-#         print( input_1 + input_2)
-#     elif input_3 == 2:
-#         print( input_1 - input_2)
-#     elif input_3 == 3:
-#         print( input_1 * input_2)
-#     elif input_3 == 4:
-#         break
-#     elif input_3 != len(opciones):
-#         print('La opcion no es correcta')
-# else:
-#     print('fin del programa')
+for i, op in enumerate(opciones):
+     print(i+1, op)
+ activar = True
+ while activar:
+     input_3 = int(input('Elija una Opcion: '))
+     if input_3 == 1:
+         print( input_1 + input_2)
+     elif input_3 == 2:
+         print( input_1 - input_2)
+     elif input_3 == 3:
+         print( input_1 * input_2)
+     elif input_3 == 4:
+         break
+     elif input_3 != len(opciones):
+         print('La opcion no es correcta')
+ else:
+     print('fin del programa')
 
 # 2) Escribí un programa que lea un número impar por teclado. Si el usuario no introduce un número impar, debe repetirse el proceso hasta que lo introduzca correctamente.
 
-# activar = True
-# while activar:
-#     input_1 = int(input('Introdusca un numero Impar: '))
-#     if input_1%2 == 0:
-#         print('Numero incorrecto')
-#     else:
-#         print('Numero correcto')
-#         break
-# else:
-#     print('fin del programa')
+ activar = True
+ while activar:
+     input_1 = int(input('Introdusca un numero Impar: '))
+     if input_1%2 == 0:
+         print('Numero incorrecto')
+     else:
+         print('Numero correcto')
+         break
+ else:
+     print('fin del programa')
 
 # 3) Escribí un programa que sume todos los números enteros impares desde el 0 hasta el 100:
 
-# lista = []
-# for impar in range(101):
-#     if impar%2 == 0:
-#         continue
-#     else:
-#         print(lista)
-#         lista.append(impar)
+ lista = []
+ for impar in range(101):
+     if impar%2 == 0:
+         continue
+     else:
+         print(lista)
+         lista.append(impar)
 
-# print(sum(lista))
+ print(sum(lista))
 
 # 4) Escribí un programa que pida al usuario cuantos números quiere introducir. Luego lee todos los números y realiza una media aritmética:
 
@@ -75,68 +75,68 @@ print('Tu medida Aritmética es: ' + str(medida_A))
 
 # 5) Escribí un programa que pida al usuario un número entero del 0 al 9, y que mientras el número no sea correcto se repita el proceso. Luego debe comprobar si el número se encuentra en la lista de números y notificarlo:
 
-# lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-# activado = True
-# while activado:
-#     input_1 = int(input('Introduce un numero del 0 al 9: '))
-#     for n in lista:
-#         if n == input_1:
-#             print('Tu numero es el: ' + str(input_1))
-#             activado = False
-#             break
-# else:
-#     print('fin del programa')
+ lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+ activado = True
+ while activado:
+     input_1 = int(input('Introduce un numero del 0 al 9: '))
+     for n in lista:
+         if n == input_1:
+             print('Tu numero es el: ' + str(input_1))
+             activado = False
+             break
+ else:
+     print('fin del programa')
 
 
 # 6) Utilizando la función range() y la conversión a listas genera las siguientes listas dinámicamente:
 
 # # Todos los números del 0 al 10 [0, 1, 2, ..., 10]
-# lista1 = []
-# for n in range(0,11):
-#     lista1.append(n)
-# print(lista1)
+ lista1 = []
+ for n in range(0,11):
+     lista1.append(n)
+ print(lista1)
 
 # # Todos los números del -10 al 0 [-10, -9, -8, ..., 0]
-# lista2 = []
-# for n in range(-10,1):
-#     lista2.append(n)
-# print(lista2)
+ lista2 = []
+ for n in range(-10,1):
+     lista2.append(n)
+ print(lista2)
 
 # # Todos los números pares del 0 al 20 [0, 2, 4, ..., 20]
-# lista3 = []
-# for n in range(0,21):
-#     if n%2 == 0:
-#         lista3.append(n)
-# print(lista3)
+ lista3 = []
+ for n in range(0,21):
+     if n%2 == 0:
+         lista3.append(n)
+ print(lista3)
 
 # # Todos los números impares entre -20 y 0 [-19, -17, -15, ..., -1]
-# lista4 = []
-# for n in range(-20,1):
-#     if n%2 != 0:
-#         lista4.append(n)
-# print(lista4)
+ lista4 = []
+ for n in range(-20,1):
+     if n%2 != 0:
+         lista4.append(n)
+ print(lista4)
 
 # # Todos los números múltiples de 5 del 0 al 50 [0, 5, 10, ..., 50]
-# lista5 = []
-# for n in range(0,51):
-#     if n%5 == 0:
-#         lista5.append(n)
-# print(lista5)
+ lista5 = []
+ for n in range(0,51):
+     if n%5 == 0:
+         lista5.append(n)
+ print(lista5)
 
 
 # 6) Dadas dos listas, debes generar una tercera con todos los elementos que se repitan en ellas, pero no debe repetirse ningún elemento en la nueva lista:
 
-# lista_1 = ["h",'o','l','a',' ', 'm','u','n','d','o']
-# lista_2 = ["h",'o','l','a',' ', 'l','u','n','a']
-# lista_3 = []
-# lista_4 = []
+ lista_1 = ["h",'o','l','a',' ', 'm','u','n','d','o']
+ lista_2 = ["h",'o','l','a',' ', 'l','u','n','a']
+ lista_3 = []
+ lista_4 = []
 
-# for l1 in lista_1:
-#     for l2 in lista_2:
-#         if l1 == l2:
-#             lista_3.append(l2)
-# for l3 in lista_3:
-#     if l3 not in lista_4:
-#         lista_4.append(l3)
+ for l1 in lista_1:
+     for l2 in lista_2:
+         if l1 == l2:
+             lista_3.append(l2)
+ for l3 in lista_3:
+     if l3 not in lista_4:
+         lista_4.append(l3)
 
-# print(lista_4)
+ print(lista_4)
