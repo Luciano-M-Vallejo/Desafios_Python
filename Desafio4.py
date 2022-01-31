137e444086fbf14ef29321b3d0e8067373db40ea
Luciano Manuel Vallejo <73795741+Luciano-M-Vallejo@users.noreply.github.com>
2022-01-31 13:39:31 -0300

    Desafio Año Bisiesto

---
+++ Desafio4.py
@@ -0,0 +1,25 @@
+# AÑO BISIESTO
+
+activar = True
+
+def ano_bisiesto(param):
+    if param%4 == 0:
+        if param%100 != 0:
+            print('Es un Año Bisiesto!!!')
+        elif param%100 == 0 and param%400 == 0:
+            print('Es un Año Bisiesto!!!')
+        else:
+            print('NO es un Año Bisiesto')
+    else:
+        print('NO es un Año Bisiesto')
+
+while activar:
+    try:
+        input_ano = int(input('Ingrese un año: '))
+        if len(str(input_ano)) == 4:
+            ano_bisiesto(input_ano)
+            activar = False
+        else:
+            print('Ingrese un numero de 4 digitos')
+    except ValueError:
+        print('Solo se permiten numeros')
